27c3f3d0ee3d8289f9d0e7777a8b64e7f56bfade
jkonvicka <konvicka.g630@gmail.com>
2023-03-31 16:00:39 +0200

    [CSP-19] dd Conversion project and Int to Ext object conversions

--- CodeGenEngine/ExtModel/PropertyExt.cs
+++ CodeGenEngine/ExtModel/PropertyExt.cs
@@ -5,5 +5,8 @@
         public string Name { get; set; }
         public DataTypeExt DataType { get; set; }
         public string DefaultValue { get; set; }
+        public AccessOperatorExt AccessOperator { get; set; }
+        public bool GenerateGetter { get; set; }
+        public bool GenerateSetter { get; set; }
     }
 }
